9f4d6c7258d0feba85e967f7208eceef4b88e9da
Robby Grossman <robby@freerobby.com>
2013-01-26 18:38:10 -0500

    New tool: Sublime Text 2 alias (subl)

--- open.bash
+++ open.bash
@@ -1,6 +1,7 @@
 TOOLS_ROOT="`dirname ${BASH_SOURCE[0]}`/tools"
 
 source "${TOOLS_ROOT}/colored-ls.bash"
-source "${TOOLS_ROOT}/prompt.bash"
 source "${TOOLS_ROOT}/git-bash-completion.bash"
+source "${TOOLS_ROOT}/prompt.bash"
 source "${TOOLS_ROOT}/rmb.bash"
+source "${TOOLS_ROOT}/subl.bash"
