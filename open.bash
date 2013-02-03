9b0b6408880864732cf37a4017fcb389d33e93ad
Robby Grossman <robby@freerobby.com>
2013-02-03 15:50:14 -0500

    New too: todo.txt-cli

--- open.bash
+++ open.bash
@@ -1,4 +1,6 @@
 TOOLS_ROOT="`dirname ${BASH_SOURCE[0]}`/tools"
+CONFIG_ROOT="`dirname ${BASH_SOURCE[0]}`/config"
+THIRDPARTY_ROOT="`dirname ${BASH_SOURCE[0]}`/third-party"
 
 source "${TOOLS_ROOT}/colored-ls.bash"
 source "${TOOLS_ROOT}/git-bash-completion.bash"
@@ -6,3 +8,5 @@ source "${TOOLS_ROOT}/postgres.bash"
 source "${TOOLS_ROOT}/prompt.bash"
 source "${TOOLS_ROOT}/rmb.bash"
 source "${TOOLS_ROOT}/subl.bash"
+
+source "${CONFIG_ROOT}/todo.txt-cli.bash"
